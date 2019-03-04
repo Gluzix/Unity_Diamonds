bbaa87363c028533724d63fa4d812239f0e00b46
Kamil <kamil.gluza@gmail.com>
2019-03-04 20:20:56 +0100

    Added Destroying of hearts

--- Assets/Scripts/Collision_with_opponent.cs
+++ Assets/Scripts/Collision_with_opponent.cs
@@ -7,11 +7,20 @@ public class Collision_with_opponent : MonoBehaviour
     private Rigidbody2D rb;
     private int how_many_opponents=4;
     private float HitForce = 35;
+    public GameObject Hearts_1, Hearts_2, Hearts_3;
+    private int heart_counter = 1;
+    private int wait_few_frames = 0;
+    private bool destroy_life = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+            destroy_life = true;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -22,6 +31,16 @@ public class Collision_with_opponent : MonoBehaviour
             {
                 rb.velocity = new Vector2();
                 rb.velocity = Vector2.up * HitForce;
+                if(destroy_life)
+                {
+                    switch(heart_counter)
+                    {
+                        case 1: Destroy(Hearts_3); heart_counter++; break;
+                        case 2: Destroy(Hearts_2); heart_counter++; break;
+                        case 3: Destroy(Hearts_1); break;
+                    }
+                    destroy_life = false;
+                }
             }
         }
     }
