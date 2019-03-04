2284b677afca16f20e2f94216f7b87940f2822d1
Kamil <kamil.gluza@gmail.com>
2019-03-04 19:48:08 +0100

    update file of collision with opponent

--- Assets/Scripts/Collision_with_opponent.cs
+++ Assets/Scripts/Collision_with_opponent.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Collision_with_opponent : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private int how_many_opponents=4;
+    private float HitForce = 40;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        for(int i=1; i<=how_many_opponents; i++)
+        {
+            if (collision.gameObject.name == "Opponent_Mace_" + i.ToString())
+            {
+                rb.velocity = new Vector2();
+                rb.velocity = Vector2.up * HitForce;
+            }
+        }
     }
 }
