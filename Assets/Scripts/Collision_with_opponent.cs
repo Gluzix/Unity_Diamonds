6e6ed1a8c79832aba1bf632717fe4715ebe4bdbd
Kamil <kamil.gluza@gmail.com>
2019-03-04 19:29:41 +0100

    Collisions with opponent

---
+++ Assets/Scripts/Collision_with_opponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_with_opponent : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
